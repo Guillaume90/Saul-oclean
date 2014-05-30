1937c48ac5c4466e983d83c17f5bcc82824613a8
lambern <lamber_n@epitech.eu>
2014-05-30 12:40:11 +0200

    first

---
+++ laravel/app/views/Accueil.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SauléoClean</title>
+	<link rel="stylesheet" type="text/css" href="./public/css/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	
+</body>
+</html>
